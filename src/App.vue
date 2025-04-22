df4e62aee91d2e81e852e87a09f12a927d10ab87
ShuShuicu <Tomori@miomoe.cn>
2025-04-23 01:05:46 +0800

    menu

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,41 @@ const router = useRouter();
 const handleMenuClick = (e) => {
   router.push(e.key);
 };
+
+const menuItems = [
+  {
+    label: '首页',
+    key: '/',
+  },
+  {
+    label: '答题',
+    key: '/answer',
+  },
+  // {
+  //   label: '人格测试',
+  //   key: '/personality',
+  // },
+  {
+    label: '关于',
+    key: '/about',
+  },
+]
 </script>
 
 <template>
   <main>
     <a-card>
-      <a-typography-title :level="2" style="text-align: center;">少女乐队浓度测试</a-typography-title>
+      <div style="text-align: center;">
+        <a-typography-title :level="2">少女乐队浓度测试</a-typography-title>
+        <a-typography-title :level="4" style="margin-top: -20px;">(TestBanGDream)</a-typography-title>
+      </div>
       <p style="text-align: center;">
         <a-typography-link href="https://space.bilibili.com/435502585" target="_blank">
           哔哩哔哩@Tomoriゞ
         </a-typography-link> | 同好群: <a-typography-text mark>574888080</a-typography-text>
       </p>
       <a-menu mode="horizontal" @click="handleMenuClick" :defaultSelectedKeys="['/']">
-        <a-menu-item key="/">首页</a-menu-item>
-        <a-menu-item key="/about">关于</a-menu-item>
+        <a-menu-item v-for="item in menuItems" :key="item.key">{{ item.label }}</a-menu-item>
       </a-menu>
       <RouterView />
     </a-card>
